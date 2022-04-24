6bb3fe7c1a2bc3513e563e9cb3fcac9a05aeb737
yumueat <1471063903@qq.com>
2022-04-24 20:02:12 +0800

    Update 锦标赛日程表.cpp

--- 算法设计与分析/实验3/锦标赛日程表.cpp
+++ 算法设计与分析/实验3/锦标赛日程表.cpp
@@ -1,6 +1,38 @@
-#include<iostream>
+#include <iostream>
+#define MAX 32
 using namespace std;
+int a[MAX][MAX];
+void recursion(int n, int pxs, int pxe, int pys, int pye, int num)
+{
+	if (n == 2)
+	{
+		a[pxs][pys] = num;
+		a[pxs][pye] = num + 1;
+		a[pxe][pys] = num + 1;
+		a[pxe][pye] = num;
+	}
+	else
+	{
+		recursion(n / 2, pxs, pxe - n / 2, pys, pye - n / 2, num);
+		recursion(n / 2, pxs, pxe - n / 2, pys + n / 2, pye, num + n / 2);
+		recursion(n / 2, pxs + n / 2, pxe, pys, pye - n / 2, num + n / 2);
+		recursion(n / 2, pxs + n / 2, pxe, pys + n / 2, pye, num);
+	}
+}
 int main()
 {
+	int n;
+	cin >> n;
+	int i, j;
+	recursion(n, 0, n - 1, 0, n - 1, 1);
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			printf("%2d",a[i][j]);
+			cout<<" "; 
+		}
+		cout << endl;
+	}
 	return 0;
 }
