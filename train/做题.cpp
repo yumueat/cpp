0820ced56ce9221de03c3f632a88a48838f91206
yumueat <1471063903@qq.com>
2022-05-25 16:12:12 +0800

    change 做题.cpp

--- train/做题.cpp
+++ train/做题.cpp
@@ -1,27 +1,7 @@
-#include<stdio.h>
-#include<string.h>
+#include<iostream>
+using namespace std;
 int main()
 {
-char s[10000];
-getchar();
-while(scanf("%[^\n]",&s)!=EOF)
-{
-fflush(stdin);
-int n=strlen(s);
-int i;
-  for(i=0; i<=n/2; i++)
-        {
-            if (s[i] != s[n-1-i])
-                break;
-        }
-        if (i > n/2)
-            {
-			printf("YES\n");}
-        else
-            {
-			printf("NO\n");}
-
-    }
-return 0;
+	cout<<"hello world"<<endl;
+	return 0;
 }
-
